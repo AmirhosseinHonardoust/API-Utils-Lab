6a59cf444c5ea2200cf2f3d03cf16784bfb1484e
Amir <88084572+AmirhosseinHonardoust@users.noreply.github.com>
2026-04-28 20:13:34 +0330

    Add headers support

--- src/api_utils/client.py
+++ src/api_utils/client.py
@@ -1,16 +1,16 @@
 import requests
 
-def get_json(url: str):
+def get_json(url: str, headers: dict = None):
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.RequestException:
         return None
 
-def post_json(url: str, data: dict):
+def post_json(url: str, data: dict, headers: dict = None):
     try:
-        response = requests.post(url, json=data)
+        response = requests.post(url, json=data, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.RequestException:
